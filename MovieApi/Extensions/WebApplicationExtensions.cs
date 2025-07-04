0255cbc327fded085cfe7827235f81d6f01e1bae
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-07-04 07:07:31 +0200

    Extension for deleteing and using SeedData.

---
+++ MovieApi/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Data;
+using System.Diagnostics;
+
+namespace MovieApi.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static async Task SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var context = serviceProvider.GetRequiredService<MovieApiContext>();
+
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.MigrateAsync();
+
+                try
+                {
+                    await SeedData.InitAsync(context);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"An error occurred while seeding the database: {ex.Message}");
+                    throw;
+                }
+            }
+        }
+    }
+}
