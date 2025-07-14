3c305facee08ea5989c040d4a1148bf08903c048
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-07-14 10:43:06 +0200

    Changes some actor related data.

--- MovieApi/Controllers/ActorsController.cs
+++ MovieApi/Controllers/ActorsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieApi.Data;
+using MovieApi.DTOs;
 using MovieApi.Models;
 
 namespace MovieApi.Controllers
@@ -18,7 +19,7 @@ namespace MovieApi.Controllers
 
         public ActorsController(MovieApiContext context)
         {
-            _context = context;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
         // GET: api/Actors
@@ -28,19 +29,35 @@ namespace MovieApi.Controllers
         //    return await _context.Actor.ToListAsync();
         //}
 
-        // GET: api/Actors/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Actor>> GetActor(int id)
-        //{
-        //    var actor = await _context.Actor.FindAsync(id);
+        //GET: api/Actors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ActorMoviesDto>> GetActor(int id)
+        {
+            var dto = await _context.Actor
+                .Where(a => a.Id == id)
+                .Select(actor => new ActorMoviesDto
+                {
+                    Id = actor.Id,
+                    Name = actor.Name,
+                    BirthYear = actor.BirthYear,
+                    Movies = actor.Movies.Select(movie => new MovieDto
+                    {
+                        Id = movie.Id,
+                        Title = movie.Title,
+                        Year = movie.Year,
+                        Genre = movie.Genre,
+                        Duration = movie.Duration,
+                        Language = movie.MovieDetails.Language
+                    })
+                })
+                .FirstOrDefaultAsync();
 
-        //    if (actor == null)
-        //    {
-        //        return NotFound();
-        //    }
+            //TODO: (update later) implement problem details.
+            if (dto == null)
+                return NotFound();
 
-        //    return actor;
-        //}
+            return Ok(dto);
+        }
 
         // PUT: api/Actors/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
@@ -75,31 +92,38 @@ namespace MovieApi.Controllers
 
         // POST: api/Actors
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //Ignore controller route. This could have been in MoviesController, but the exercise requires it to be here.
         [Route("~/api/movies/{movieId}/actors/{actorId}")]
         [HttpPost]
-        public async Task<ActionResult<Actor>> AddActorToMovie([FromRoute] int movieId, [FromRoute] int actorId)
+        public async Task<ActionResult<ActorDto>> AddActorToMovie([FromRoute] int movieId, [FromRoute] int actorId)
         {
-            //Check if the movie exists
+            //Check if the movie exists, and fetch it
             var movie = await _context.Movie
-                .Where(m => m.Id == movieId)
-                .FirstOrDefaultAsync();
+                .FirstOrDefaultAsync(m => m.Id == movieId);
 
             if (movie == null) 
                 return NotFound("Movie not found.");
 
-            //Check if the actor exists
+            //Check if the actor exists, and fetch it
             var actor = await _context.Actor
-                .Where(a => a.Id == actorId)
-                .FirstOrDefaultAsync();
+                .FirstOrDefaultAsync(a => a.Id == actorId);
 
             if (actor == null)
-                return NotFound("Actor not found.");    
+                return NotFound("Actor not found.");   
+            
+            //This is for the return.
+            ActorDto actorDto = new ActorDto
+            {
+                Id = actor.Id,
+                Name = actor.Name,
+                BirthYear = actor.BirthYear
+            };
 
             movie.Actors.Add(actor);
 
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetActor", new { id = actor.Id }, actor);
+            return CreatedAtAction("GetActor", new { id = actor.Id }, actorDto);
         }
 
         // DELETE: api/Actors/5
--- MovieApi/DTOs/ActorMoviesDtoDto.cs
+++ MovieApi/DTOs/ActorMoviesDtoDto.cs
@@ -1,10 +1,11 @@
 ﻿namespace MovieApi.DTOs
 {
-    public class ActorDto
+    public class ActorMoviesDto
     {
         //Id is included for future manipulation.
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public int BirthYear { get; set; }
+        public IEnumerable<MovieDto> Movies { get; set; } = new List<MovieDto>();
     }
 }
